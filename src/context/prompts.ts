143ecdc4650472e7452f8f5d1b6216f4cc31e5c4
steven arellano <stevenjarellano2@gmail.com>
2023-03-02 14:06:31 -0600

    fixed dashes

--- src/context/prompts.ts
+++ src/context/prompts.ts
@@ -1,5 +1,5 @@
 const PROMPTS = [
-	'What transforms this world is — knowledge. Do you see what I mean? Nothing else can change anything in this world.',
+	'What transforms this world is - knowledge. Do you see what I mean? Nothing else can change anything in this world.',
 	'Knowledge alone is capable of transforming the world, while at the same time leaving it exactly as it is.',
 	'When you look at the world with knowledge, you realize that things are unchangeable and at the same time are constantly being transformed.',
 ];
