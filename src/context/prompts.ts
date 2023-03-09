f58a6e1087a95bc8622b348a5105e626d9f988fb
steven arellano <stevenjarellano2@gmail.com>
2023-03-09 12:02:09 -0600

    fixed prompt

--- src/context/prompts.ts
+++ src/context/prompts.ts
@@ -1,5 +1,5 @@
 const PROMPTS = [
-	"I wanna be the very best. Like no one ever was. To catch them is my real test. To train them is my cause. I will travel across the land. Searching far and wide. Teach Pokémon to understand. The power that's inside",
+	"I wanna be the very best. Like no one ever was. To catch them is my real test. To train them is my cause. I will travel across the land. Searching far and wide. Teach Pokemon to understand. The power that's inside",
 	"I don't want a lot for Christmas. There is just one thing I need. I don't care about the presents underneath the Christmas tree. I just want you for my own. More than you could ever know. Make my wish come true. All I want for Christmas is you",
 	'When you look at the world with knowledge, you realize that things are unchangeable and at the same time are constantly being transformed.',
 	"I'ma get a scholarship to King's College. I probably shouldn't brag, but dang, I amaze and astonish. The problem is I got a lot of brains but no polish",
